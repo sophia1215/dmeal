89e5c2d23f05c5424268f2c3f4c7f1852472e8a0
sophia1215 <code.sophiawang@gmail.com>
2016-01-23 16:12:55 +0800

    CarrierWave, MiniMagick, ImageMagick install && Image Uploading

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,13 @@
+CarrierWave.configure do |config|
+  module CarrierWave
+    module MiniMagick
+      def quality(percentage)
+        manipulate! do |img|
+          img.quality(percentage.to_s)
+          img = yield(img) if block_given?
+          img
+        end
+      end
+    end
+  end
+end
